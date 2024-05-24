b15cc00354dbc602c756e25abe66dd0fb9f14e77
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2024-05-24 10:28:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app.pedibus',
+  webDir: 'www'
+};
+
+export default config;
